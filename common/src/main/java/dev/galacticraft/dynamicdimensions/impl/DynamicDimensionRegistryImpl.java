0e40cda4c5344f58fc752c2b8030dbbf849b0e09
Marvin Eckhardt <marvineckhardt04092003@gmail.com>
2025-11-03 13:27:54 +0100

    using primary level data for custom time / weather

--- common/src/main/java/dev/galacticraft/dynamicdimensions/impl/DynamicDimensionRegistryImpl.java
+++ common/src/main/java/dev/galacticraft/dynamicdimensions/impl/DynamicDimensionRegistryImpl.java
@@ -47,15 +47,18 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.tags.TagKey;
 import net.minecraft.tags.TagManager;
 import net.minecraft.tags.TagNetworkSerialization;
+import net.minecraft.world.RandomSequences;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.ForcedChunksSavedData;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelSettings;
 import net.minecraft.world.level.biome.BiomeManager;
 import net.minecraft.world.level.border.BorderChangeListener;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.dimension.LevelStem;
-import net.minecraft.world.level.storage.DerivedLevelData;
+import net.minecraft.world.level.levelgen.WorldOptions;
+import net.minecraft.world.level.storage.PrimaryLevelData;
 import net.minecraft.world.level.storage.WorldData;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -163,20 +166,34 @@ public class DynamicDimensionRegistryImpl implements DynamicDimensionRegistry {
 
     private @NotNull ServerLevel createDynamicLevel(ResourceKey<Level> key, WorldData worldData, LevelStem stem, ServerLevel overworld) {
         // -- start createLevels --
-        final DerivedLevelData data = new DerivedLevelData(worldData, worldData.overworldData()); //todo: do we want separate data?
+       PrimaryLevelData levelData = new PrimaryLevelData(
+                new LevelSettings(
+                        worldData.getLevelName()+"_"+key.location().getPath(),
+                        worldData.getGameType(),
+                        worldData.isHardcore(),
+                        worldData.getDifficulty(),
+                        worldData.isAllowCommands(),
+                        worldData.getGameRules(),
+                        worldData.getDataConfiguration()
+                ),
+                new WorldOptions(0,false,false),        // the serverlevel does not care what you provide
+                PrimaryLevelData.SpecialWorldProperty.NONE,
+                worldData.worldGenSettingsLifecycle()
+        );
+
         final ServerLevel level = new ServerLevel(
                 this.server,
                 ((MinecraftServerAccessor) this.server).getExecutor(),
                 ((MinecraftServerAccessor) this.server).getStorageSource(),
-                data,
+                levelData,
                 key,
                 stem,
                 ((MinecraftServerAccessor) this.server).getProgressListenerFactory().create(10),
                 worldData.isDebugWorld(),
-                BiomeManager.obfuscateSeed(worldData.worldGenOptions().seed()),
+                BiomeManager.obfuscateSeed(overworld.getSeed() + key.location().hashCode()),
                 ImmutableList.of(),
-                false,
-                null
+                true,
+                new RandomSequences(overworld.getSeed() + key.location().hashCode())
         );
         overworld.getWorldBorder().addListener(new BorderChangeListener.DelegateBorderChangeListener(level.getWorldBorder()));
         // -- end createLevels --
